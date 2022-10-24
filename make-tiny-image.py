19e412a6b658c2aacc6a5c9f82edc38291b6c6af
Daniel P. Berrangé <berrange@redhat.com>
2022-10-24 14:54:52 +0100

    Add support for loading kmods
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- make-tiny-image.py
+++ make-tiny-image.py
@@ -30,7 +30,7 @@ def which(exe):
     else:
         raise Exception("Cannot find '%s' in '%s'" % (exe, path))
 
-def make_busybox(tmpdir, runcmd):
+def make_busybox(tmpdir, runcmd, loadmods):
     bin = os.path.join(tmpdir, "bin")
     os.makedirs(bin, exist_ok=True)
 
@@ -65,8 +65,12 @@ mknod -m 666 /dev/tty c 5 0
 mknod -m 666 /dev/ttyS0 c 4 64
 mknod -m 444 /dev/random c 1 8
 mknod -m 444 /dev/urandom c 1 9
+""", file=fh)
 
-%s
+        for mod in loadmods:
+            print("insmod %s" % mod, file=fh)
+
+        print("""%s
 poweroff -f
 """ % runcmd, file=fh)
     os.chmod(init, stat.S_IRWXU)
@@ -133,8 +137,61 @@ def make_binaries(tmpdir, binaries):
 
     install_deps(tmpdir, binaries)
 
-def make_image(tmpdir, output, copyfiles, binaries, runcmd):
-    make_busybox(tmpdir, runcmd)
+
+def kmod_deps(modfile):
+    out = subprocess.check_output(["modinfo", modfile], stderr=subprocess.STDOUT).decode("utf8")
+    for line in out.split("\n"):
+        if line.startswith("depends: "):
+            deps = line[8:].strip()
+            if deps == "":
+                return []
+            return [a.replace("-", "_") for a in deps.split(",")]
+
+
+def copy_kmod(tmpdir, kmoddir, allmods, mod):
+    src = os.path.join(kmoddir, allmods[mod])
+    dstdir = os.path.join(tmpdir, "lib", "modules")
+    if not os.path.exists(dstdir):
+        os.makedirs(dstdir)
+    dst = os.path.join(dstdir, os.path.basename(allmods[mod]))
+    if os.path.exists(dst):
+        return
+    print("Copy kmod %s -> %s" % (src, dst))
+    copy(src, dst)
+
+    loadmods = []
+    for depmod in kmod_deps(src):
+        loadmods.extend(copy_kmod(tmpdir, kmoddir, allmods, depmod))
+
+    loadmods.append(os.path.join("/lib", "modules",
+                                 os.path.basename(allmods[mod])))
+    return loadmods
+
+
+def make_kmods(tmpdir, kmods, kver):
+    print(kver)
+    kmoddir = os.path.join("/lib", "modules", kver, "kernel")
+    if not os.path.exists(kmoddir):
+        raise Exception("kmod dir '%s' does not exist" % kmoddir)
+
+    allmods = {}
+    for path in glob.glob("**/*.ko*", root_dir=kmoddir, recursive=True):
+        mod = os.path.basename(path).split(".")[0]
+        mod = mod.replace("-", "_")
+        allmods[mod] = path
+
+    loadmods = []
+    for mod in kmods:
+        if mod not in allmods:
+            raise Exception("kmod '%s' does not exist" % mod)
+        loadmods.extend(copy_kmod(tmpdir, kmoddir, allmods, mod))
+    return loadmods
+
+def make_image(tmpdir, output, copyfiles, kmods, kver, binaries, runcmd):
+    loadmods = make_kmods(tmpdir, kmods, kver)
+    make_busybox(tmpdir, runcmd, loadmods)
+    if len(loadmods) > 0 and "insmod" not in binaries:
+        binaries.append("insmod")
     make_binaries(tmpdir, binaries)
 
     for copyfileglob in copyfiles:
@@ -167,6 +224,10 @@ parser.add_argument('--run', default="setsid cttyhack /bin/sh",
                     help='Command to execute in guest (default: "setsid cttyhack /bin/sh")')
 parser.add_argument('--copy', action="append", default=[],
                     help='Extra files to copy  /src=/dst')
+parser.add_argument('--kmod', action="append", default=[],
+                    help='Kernel modules to load')
+parser.add_argument('--kver', default=os.uname().release,
+                    help='Kernel version to add modules for')
 parser.add_argument('binary', nargs="*",
                     help='List of binaries to include')
 
@@ -175,4 +236,5 @@ args = parser.parse_args()
 print (args.output)
 
 with TemporaryDirectory(prefix="make-tiny-image") as tmpdir:
-    make_image(tmpdir, args.output, args.copy, args.binary, args.run)
+    make_image(tmpdir, args.output, args.copy,
+               args.kmod, args.kver, args.binary, args.run)
